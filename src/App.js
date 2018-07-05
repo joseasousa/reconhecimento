bb5e1ccdab0fb545b0fc9d6e881cab540e131393
jose alves sousa neto <a.sousajose@gmail.com>
2018-07-05 17:33:01 -0400

    adicionar Travis CI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import Traking from './Traking'
 import NavBar from './component/NavBar'
-import './App.css'
 
 class App extends Component {
   constructor (props) {
